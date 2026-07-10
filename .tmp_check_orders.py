d544f77cc22770e9fe70e46e03c9559827b6e60b
Kenneth James Boncales <kennethboncales608@gmail.com>
2026-07-11 02:09:47 +0800

    chore: remove temporary scripts and update gitignore

--- .tmp_check_orders.py
+++
@@ -1,6 +0,0 @@
-import sqlite3, json
-conn = sqlite3.connect('backend/db.sqlite3')
-cur = conn.cursor()
-cur.execute('SELECT order_id, customer, created_at FROM api_order ORDER BY created_at DESC LIMIT 5')
-rows = cur.fetchall()
-print(json.dumps(rows, default=str, indent=2))
