3f253622356aa2eefcb09672a93432bdac94b6a0
Kenneth James Boncales <kennethboncales608@gmail.com>
2026-07-30 11:10:57 +0800

    added script + analytics and fix file structure

--- enhancement/backend/scripts/extraction.bat
+++ enhancement/backend/scripts/extraction.bat
@@ -1,10 +1,11 @@
 @echo off
-cd /d %~dp0 
+cd /d %~dp0
 wsl -d Ubuntu -e sudo service clickhouse-server start
 
 echo Waiting for ClickHouse to be ready...
 timeout /t 5 /nobreak >nul
 
+if not exist ..\logs mkdir ..\logs
 
-cd /d C:\Django\Cutwise-IMS\enhancement
-analytics-venv\Scripts\python.exe script.py >> logs\etl_run.log 2>&1
+echo Running extraction script...
+..\..\analytics-venv\Scripts\python.exe extraction.py >> ..\logs\extraction.log 2>&1
