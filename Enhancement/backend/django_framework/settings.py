0c54158d8932afb8940c4b29827e273cbf1e1569
Kenneth James Boncales <kennethboncales608@gmail.com>
2026-08-01 16:38:43 +0800

    feature: added new CORS to for local run

--- Enhancement/backend/django_framework/settings.py
+++ Enhancement/backend/django_framework/settings.py
@@ -106,6 +106,8 @@ STATIC_URL = '/static/'
 # CORS - allow local React dev server
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
+    'http://localhost:5174',
+    'http://127.0.0.1:5174',
     'http://localhost:5175',
     'http://127.0.0.1:5175',
     'http://localhost:5176',
